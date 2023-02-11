da102ba7b271ed8134b40f5ffe276e6358c2923b
shadow3731 <v1rus3731@gmail.com>
2023-02-11 16:06:06 +0300

    commit changes

---
+++ src/main/java/local/chat/springchattest/SpringChatTestApplication.java
@@ -0,0 +1,13 @@
+package local.chat.springchattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/chat/springchattest/SpringChatTestApplicationTests.java
@@ -0,0 +1,13 @@
+package local.chat.springchattest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
