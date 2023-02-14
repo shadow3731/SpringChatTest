443dc3bc2001ea55c7191de760bb1afb7bec7026
shadow3731 <v1rus3731@gmail.com>
2023-02-14 16:56:44 +0300

    commit changes

--- src/main/java/local/chat/springchattest/controller/ChatController.java
+++ src/main/java/local/chat/springchattest/controller/ChatController.java
@@ -3,10 +3,12 @@ package local.chat.springchattest.controller;
 import local.chat.springchattest.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class UsersController {
+public class ChatController {
 
     private UsersService usersService;
 
@@ -15,10 +17,14 @@ public class UsersController {
         this.usersService = usersService;
     }
 
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
 
-    @GetMapping("/users/count")
-    public int countUsers() {
-        System.out.println(usersService.countAllUsers());
-        return usersService.countAllUsers();
+    @ModelAttribute
+    public void countUsers(Model model) {
+        model.addAttribute("totalUsers",
+                usersService.countAllUsers());
     }
 }
